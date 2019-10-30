6e87df4c2fe2e2f22089a369c5ee5a94a6ff1365
INazca <billy78@gmx.net>
2019-10-30 12:15:02 +0100

    Enable file input

--- download/src/index.js
+++ download/src/index.js
@@ -2,9 +2,9 @@ const {Command, flags} = require('@oclif/command')
 
 class DownloadCommand extends Command {
   async run() {
-    const {flags} = this.parse(DownloadCommand)
-    const name = flags.name || 'world'
-    this.log(`hello ${name} from .\\src\\index.js`)
+    const {args} = this.parse(DownloadCommand)
+
+    downloadFile(`${process.cwd()}\\${args.file}`)
   }
 }
 
@@ -13,12 +13,25 @@ DownloadCommand.description = `Describe the command here
 Extra documentation goes here
 `
 
+// specifiy arguments for the command here
+DownloadCommand.args = [
+  {
+    name: 'file',
+    required: true,
+  },
+]
+
+// specify flags of the command here
 DownloadCommand.flags = {
   // add --version flag to show CLI version
   version: flags.version({char: 'v'}),
   // add --help flag to show CLI version
   help: flags.help({char: 'h'}),
   name: flags.string({char: 'n', description: 'name to print'}),
+}
+
+function downloadFile(path){
+  console.log(path)
 }
 
 module.exports = DownloadCommand
